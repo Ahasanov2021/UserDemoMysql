10337ad8dc761046efe113112dffa3dff5906946
Ali Hasanov <ahasanov4444@gmail.com>
2024-07-12 18:06:36 +0400

    db changelog prod and test added yaml edited

--- src/main/java/az/ingress/demo/DemoApplication.java
+++ src/main/java/az/ingress/demo/DemoApplication.java
@@ -2,14 +2,24 @@ package az.ingress.demo;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class DemoApplication{
+public class DemoApplication implements CommandLineRunner {
+
+	@Value("${spring.profiles.active}")
+	private String profile;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(profile);
 	}
 }
---
+++ src/main/resources/db/changelog/sql/a.sql
@@ -0,0 +1,18 @@
+insert into `user` (
+    age,
+    balance,
+    email,
+    is_active,
+    lastname,
+    name,
+    `password`,
+    phone)
+values (
+    23,
+    1200,
+    'hasan@gmail.com',
+    true,
+    'Hasanov',
+    'Ali',
+    'password',
+    '44598798987987');
